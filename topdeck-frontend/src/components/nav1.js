f27a1d35a2f1b35f5f68c835861844272fc4419c
AmandaFrisk <amanda.frisk33@gmail.com>
2022-09-29 11:15:43 -0600

    changed div to nav tag in nav1

--- topdeck-frontend/src/components/nav1.js
+++ topdeck-frontend/src/components/nav1.js
@@ -1,7 +1,8 @@
 //Nav for View-My-Decks page -AF
 const Nav1 = () => {
     return (
-        <div className='nav1'>
+        <nav className='nav1'>
+        
              <div className='logo-nav1-div'>
 {/* Need to resize image -AF */}
                 <img className ='logo-mg'src='/images/Logo.png' alt='Top Deck Logo'/>
@@ -10,7 +11,8 @@ const Nav1 = () => {
 {/* Need to add route or whatever we end up doing to get user to Create A Deck page/pop-out- AF*/}
             <a href ='#'>CREATE A DECK</a>
             </div>
-        </div>
+        
+        </nav>
     )
 }
 
