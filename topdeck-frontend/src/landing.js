2f239cdccb377f6c024bc174954377c3a391fc40
AmandaFrisk <amanda.frisk33@gmail.com>
2022-09-29 10:55:01 -0600

    added the following: nav1, landing and empty view-my-decks.js

--- topdeck-frontend/src/landing.js
+++ topdeck-frontend/src/landing.js
@@ -1,7 +1,23 @@
+//imports
+import './App.css';
+import './index.css';
+
+// idea is to have handleClick direct user to view-my-decks page-AF
+const handleClick = ()=>{
+console.log('button clicked')
+}
 const Landing = () => {
     return (
         <div className='landing'>
-    <h1>Landing Page</h1>
+             <h1>Top Deck</h1>
+            <div className='landing-logo-div'>
+                <img src='/images/Logo.png' alt='Top Deck Logo'/>
+            </div>
+             <div className='landing-info-div'>
+            <h2>Build your own Pokemon decks from a database of Pokemon cards!
+                This includes cards from the base deck through Sword and Shield.</h2>
+            </div>
+            <button onClick={handleClick}>CLICK HERE TO GET STARTED</button>
         </div>
     )
 }
