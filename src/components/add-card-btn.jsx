ef11d52005b8277ec13cc14aea21ac6736d0e3c1
danielnathaniel <danielyaghoobian@email.com>
2022-10-04 21:37:13 -0700

    deleting a card now works

--- src/components/add-card-btn.jsx
+++ src/components/add-card-btn.jsx
@@ -1,13 +1,10 @@
+
 // below is our functional component 
 const AddCardBtn = () => ( 
    <div>
 Add a new pokemon card!
    </div>
 )
-
-
-
-
 
 
 export default add-card-btn
--- src/view-single-deck.js
+++ src/view-single-deck.js
@@ -5,8 +5,16 @@ import Nav2 from './components/nav2';
 import DeckList from './components/deck-list';
 import Delete from './components/delete-card';
 import UpdateDeckBtn from './components/update-deck-btn';
+import baseUrl from "./baseUrl";
 
 class Deck extends Component {
+  handleClick=(cardId)=>{
+    fetch(`${baseUrl}/cards/${cardId}`, {
+      method: "DELETE"
+    })
+      .then((response) => response.json())
+      .then((data) => console.log(data));
+  }
  render(){
  return (
     <>
@@ -17,7 +25,14 @@ class Deck extends Component {
     </div>
    {/* Delete needs to be applied to each card */}
    <div>
-    <Delete />
+
+<Delete onClick={
+    ()=>
+      this.handleClick("cardId") 
+      // take off the quotes on cardId above
+    
+    } /> 
+
    </div>
    <div>
     <DeckList />
