d9516a576eaf5dcb170b28c46e1e57333f94638c
AmandaFrisk <amanda.frisk33@gmail.com>
2022-10-05 18:01:24 -0600

    updated card view

--- src/view-my-decks.js
+++ src/view-my-decks.js
@@ -3,7 +3,7 @@ import React, {Component} from 'react'
 import './index.css';
 import Nav1 from './components/nav1'
 import Decks from './components/my-decks';
- 
+
 class List extends Component {
  render(){
  return (
